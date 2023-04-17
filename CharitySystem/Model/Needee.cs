dbff914a44b7f807c82f3b8428fffbc447305b8f
ViraAroonRayka <viraaroonrayka@gmail.com>
2023-04-17 17:35:42 +0430

    Upload Project

---
+++ CharitySystem/Model/Needee.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CharitySystem.Model
+{
+    public class Needee
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+        public string FirstName
+        {
+            get;
+            set;
+        }
+        public string LastName
+        {
+            get;
+            set;
+        }
+        public string Phone
+        {
+            get
+            {
+                return Phone;
+            }
+            set
+            {
+                if(value.Length == 11)
+                {
+                    Phone = value;
+                }
+            }
+        }
+        public string NationalCode
+        {
+            get;
+            set;
+        }
+        public List<Skill> skills
+        {
+            get;
+            set;
+        }
+        public string Graduation
+        {
+            get;
+            set;
+        }
+        public Province province
+        {
+            get;
+            set;
+        }
+        public City city
+        {
+            get;
+            set;
+        }
+        
+    }
+}
